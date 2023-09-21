138fec59ec7d51d135fe1de847e8c486352e0d80
Neeraj Sahita <neerajsahita@Neerajs-MacBook-Air.local>
2023-09-21 18:17:57 +0530

    Salting and Hashing Level4

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { userInfo } = require("os");
 // const encrypt = require("mongoose-encryption"); 
-const md5 = require("md5"); 
+// const md5 = require("md5"); 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -40,26 +42,32 @@ app.get("/register", (req,res)=>{
 })
 
 app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    }); 
-    newUser.save().then(res.render("secrets"));
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        }); 
+        newUser.save().then(res.render("secrets"));
+    });
 });
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username}).then(function(foundUser, error){
         if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");
-            }
-            else{
-                // console.log(error);
-                res.render("home"); 
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                // result == true
+                if(result===true){
+                    res.render("secrets");
+                }
+                else{
+                    // console.log(error);
+                    res.render("home"); 
+                }
+            });
         }
         else{
             console.log(error);
